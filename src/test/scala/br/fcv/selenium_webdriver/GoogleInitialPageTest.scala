697cf24249189379c51a4418b97f2cf266eb9b8d
Fabio Veronez <fabio.veronez@gmail.com>
2011-06-13 09:35:18 -0300

    renaming test so its executed by maven test phase

---
+++ src/test/scala/br/fcv/selenium_webdriver/GoogleInitialPageTest.scala
@@ -0,0 +1,29 @@
+package br.fcv.selenium_webdriver
+
+import org.junit.runner.RunWith
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.{By, NoSuchElementException => NoSuchWebElementException}
+import org.openqa.selenium.WebDriver
+import org.scalatest.fixture.FixtureFunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+import scala.collection.JavaConversions._
+import scala.util.control.Breaks._
+import support.RichWebDriver.conversions._
+
+@RunWith(classOf[JUnitRunner])
+class GoogleInitialPageTest extends WebDriverFixtureFunSuite with ShouldMatchers {
+    
+    test("testing") { driver =>
+        driver get "http://www.google.com.br"
+        
+        val elm = driver \ By.id("main") \ By.id("body") \ By.id("lga") \ By.name("name-not-found") \ By.name("test")
+        
+        val exception = intercept[NoSuchWebElementException] {
+            elm !
+        }        
+        
+        exception.getMessage should (include ("name-not-found") and not include ("teste"))
+    }
+}
