631c3b3dc0b1d4ead4105e436e0c23ba3e7a9443
Fabio Veronez <fabio.veronez@gmail.com>
2011-06-14 17:04:42 -0300

    first implementation of waiting test

--- src/main/scala/br/fcv/selenium_webdriver/support/RichWebDriver.scala
+++ src/main/scala/br/fcv/selenium_webdriver/support/RichWebDriver.scala
@@ -1,11 +1,20 @@
 package br.fcv.selenium_webdriver.support
 
+import com.google.common.base.Function
 import org.openqa.selenium.{By, WebDriver}
 import org.openqa.selenium.{NoSuchElementException => NoSuchHtmlElementException}
+import org.openqa.selenium.support.ui.WebDriverWait
 
 class RichWebDriver(driver: WebDriver) extends RichSearchContext(driver)  {   
     
    override val root = Nil
+   
+   def waitUntil[T](timeout: Long, sleepTime: Long = 1000)(cond: WebDriver => T): T = {
+       val wait = new WebDriverWait(driver, timeout, sleepTime);
+       wait.until(new Function[WebDriver, T] {
+           def apply(driver: WebDriver) = cond(driver)
+       });
+   }   
 
 }
 
---
+++ src/test/scala/br/fcv/selenium_webdriver/WaitUntilTest.scala
@@ -0,0 +1,37 @@
+package br.fcv.selenium_webdriver
+
+import org.scalatest.matchers.ShouldMatchers
+import br.fcv.selenium_webdriver.support.implicits._
+import org.openqa.selenium.By.{className, id, name, tagName}
+import org.openqa.selenium.{ NoSuchElementException => NoSuchWebElementException }
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.By
+
+@RunWith(classOf[JUnitRunner])
+class WaitUntilTest extends WebDriverFixtureFunSuite with ShouldMatchers {
+
+    test("testing failure") { driver =>
+        
+        val button = driver \ id("button")        
+        button.click
+        
+        intercept[NoSuchWebElementException] {
+        	val ul = driver \ id("my-ul") !
+        }
+    }
+    
+    test("testing waiting") { driver =>
+        
+        val button = driver \ id("button")        
+        button.click
+        
+        val ul = driver.waitUntil(2000) { d => d \ id("my-ul") ! }
+        
+        
+        val lis = ul findElements By.tagName("li")
+        lis.size should be === 10
+    }
+    
+}
