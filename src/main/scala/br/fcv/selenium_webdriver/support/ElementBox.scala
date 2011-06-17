80f616ccc5c4715c226fdfe3e0c3fb33707d0e71
Fabio Veronez <fabio.veronez@gmail.com>
2011-06-17 16:33:05 -0300

    just adding some scaladocs

--- src/main/scala/br/fcv/selenium_webdriver/support/ElementBox.scala
+++ src/main/scala/br/fcv/selenium_webdriver/support/ElementBox.scala
@@ -6,6 +6,22 @@ import org.openqa.selenium.By
 
 import scala.collection.JavaConversions._
 
+/** 
+ * Abstraction very similar to [[scala.Option]] but this implementation
+ * holds a reference to all known previously used <code>WebElement</code>s so
+ * that better error message could be shown when an <code>NoSuchElementException</code>
+ * is throw. Ex: 
+ * 
+ * {{{
+ * val elBox: ElementBox = driver \ By.id("my-id") \ By.name("my-name") \ By.id("unkown-id")
+ * val el: WebElement = elBox.get //- NoSuchElementException thrown 
+ * }}}
+ * 
+ * In the example above <code>NoSuchElementException</code> will carry a message saying 
+ * that <code>unkown-id</code> was not found under <code>\ By.id("my-id") \ By.name("my-name")</code> instead of 
+ * only saying that <code>unkown-id</code> was not found.
+ * 
+ */
 sealed abstract class ElementBox(path: List[WebElement]) {
     
     def get: WebElement
@@ -39,6 +55,11 @@ sealed abstract class ElementBox(path: List[WebElement]) {
     
 }
 
+/**
+ * Represents an empty [[br.fcv.selenium_webdriver.support.ElementBox]]
+ * 
+ * Note that every access to <code>get</code> method will throw <code>org.openqa.selenium.NoSuchElementException</code>
+ */
 final class Empty(path: List[WebElement], by: By) extends ElementBox(path) {
     def get = throw new NoSuchWebElementException( (path map printWebElement).mkString("\\") + " \\ " + by )
     val isEmpty = true
@@ -61,6 +82,9 @@ object Empty {
     def unapply(e: Empty) = true
 }
 
+/**
+ * An [[br.fcv.selenium_webdriver.support.ElementBox]] which holds an WebElement.
+ */
 final class Full(path: List[WebElement], current: WebElement) extends ElementBox(path :+ current) {
     def get = current
     val isEmpty = false
