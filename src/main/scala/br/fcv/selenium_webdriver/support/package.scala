6611a08f9700cc69d1d69a8e29d30c664c8aa0a9
Fabio Veronez <fabio.veronez@gmail.com>
2011-06-22 14:20:19 -0300

    changing box2element implicit method so it just return webelement that is hold by ElementBox instead of creating another instance

--- src/main/scala/br/fcv/selenium_webdriver/support/package.scala
+++ src/main/scala/br/fcv/selenium_webdriver/support/package.scala
@@ -10,26 +10,7 @@ package object support {
         implicit def enrichWebElement(element: WebElement) = new RichWebElement(element);
         implicit def enrichWebDriver(driver: WebDriver) = new RichWebDriver(driver);
 
-        implicit def box2element(box: ElementBox) = new WebElement {
-            private def elm = box !
-            def click = elm click
-            def submit = elm submit            
-            def sendKeys(keysToSend: CharSequence*) = elm sendKeys (keysToSend: _*);
-            def clear = elm clear
-            def getTagName = elm getTagName;
-            def getAttribute(name: String) = elm getAttribute name;
-            def toggle() = elm toggle;
-            def isSelected = elm isSelected;
-            def setSelected() = elm setSelected;
-            def isEnabled = elm isEnabled;
-            def getText = elm getText;
-            def findElements(by: By) = elm findElements by;
-            def findElement(by: By) = elm findElement by;
-            def isDisplayed = elm.isDisplayed;
-            def getLocation = elm.getLocation;
-            def getSize = elm.getSize;
-            def getCssValue(propertyName: String) = elm getCssValue propertyName
-        }
+        implicit def box2element(box: ElementBox): WebElement = box.get
     }
 
 }
